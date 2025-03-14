f2b3b88c205450bef8d57b06f2a5db40fb8150f2
DarkfireLycaon <rafatorretta@gmail.com>
2025-03-14 19:34:32 +0100

    config

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from . import views
-
+from django.urls import path, include
+from django.contrib import admin
+from blog.views import CreateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
-    path('api/hello/', views.hello_world),
+    path('admin/', admin.site.urls),
+    path('api/user/register', CreateUserView.as_view(), name='register'),
+    path('api/token/', TokenObtainPairView.as_view(), name='get_token'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh'),
+    path('api-auth/', include('rest_framework.urls')),
 ]
